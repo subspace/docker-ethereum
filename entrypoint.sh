255ea63a848d9ab60a9390b0347a0da2e03464e5
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2019-02-10 15:57:46 +0200

    Make image more generic with support for arbitrary arguments to `geth`

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ mkdir -p /home/ethereum/.ethereum
 chmod 700 /home/ethereum/.ethereum
 chown -R ethereum /home/ethereum/.ethereum
 
-exec gosu ethereum geth --rpc --rpcaddr 0.0.0.0 --ws --wsaddr 0.0.0.0 --syncmode fast
+exec gosu ethereum geth $@
